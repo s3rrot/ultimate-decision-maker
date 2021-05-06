b9ea426db660ef34c90c14f6228b4047071e0057
Fer Torres <s3rrot@users.noreply.github.com>
2021-05-06 18:07:46 +0100

    Fix deploy script source branch

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:s3rrot/ultimate-decision-maker.git master:gh-pages
+git push -f git@github.com:s3rrot/ultimate-decision-maker.git main:gh-pages
 cd -
